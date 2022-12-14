d6c429a385bbaf6145b98dd70aa5eff757ec9505
Ale Rangel <31944093+alecy1@users.noreply.github.com>
2022-12-13 20:31:29 -0600

    Escribi un comentario explicando el objetivo, de  cada funcion del archivo.

--- funcionalidadJS/funciones.js
+++ funcionalidadJS/funciones.js
@@ -1,5 +1,6 @@
 
-/*El objetivo de la funcion insertaInformacion(), es generar los registros de los items*/
+/*El objetivo de la funcion insertaInformacion(), es generar los registros de los items, 
+para lograr el registro se utiliza la codificacion del archivo insertaInfo.php*/
   function insertaInformacion() {
     let item = document.forms["myForm"]["item"].value;
       if (item == "") {
@@ -13,12 +14,13 @@
         xhttp.open("POST", "./funcionalidadPHP/insertaInfo.php?q="+item);
         xhttp.send();
       }
-    }
+  }
  
 
-/*El objetivo de la funcion retornaInformacion(), es mostrar la lista de items rwgistrados. */
-    function retornaInformacion() {
-      var lista = document.getElementById("mostrar").value;
+/*El objetivo de la funcion retornaInformacion(), es mostrar la lista de items registrados.
+Esta funcion se comunica con el archivo retornarInfo.php*/
+  function retornaInformacion() {
+    var lista = document.getElementById("mostrar").value;
       if (lista.length == 0) { 
         document.getElementById("mostrarInformacion").innerHTML = "";
         return;
@@ -29,16 +31,17 @@
       }
       xhttp.open("POST", "./funcionalidadPHP/retornarInfo.php?q="+lista);
       xhttp.send();
-    }
+  }
 
-/*El objetivo de la funcion eliminaLista(), es eliminar los registros de los items */
-    function eliminaLista(){
-      var elimina = document.getElementById("eliminar").value;
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function(){
-        document.getElementById("mostrarInformacion").innerHTML=this.responseText;
-      }
-      xhttp.open("POST", "./funcionalidadPHP/retornarInfo.php?q="+elimina);
-      xhttp.send();
+/*El objetivo de la funcion eliminaLista(), es eliminar los registros de los items, 
+para lograr esto la funcion, llama al archivo retornarInfo.php*/
+  function eliminaLista(){
+    var elimina = document.getElementById("eliminar").value;
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+      document.getElementById("mostrarInformacion").innerHTML=this.responseText;
     }
+    xhttp.open("POST", "./funcionalidadPHP/retornarInfo.php?q="+elimina);
+    xhttp.send();
+  }
 
