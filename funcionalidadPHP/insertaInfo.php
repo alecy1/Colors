3b9eb9652fb4857f85f1ba6275f0bf1ad0f8ebe8
Ale Rangel <31944093+alecy1@users.noreply.github.com>
2022-12-13 20:43:08 -0600

    Agregue comentarios sobre la funcionalidad del codigo.

--- funcionalidadPHP/insertaInfo.php
+++ funcionalidadPHP/insertaInfo.php
@@ -1,17 +1,20 @@
 <?php
+/*En la variable $mysqli guardo la conexion de la base de datos y compruebo si hay error de conexion con 
+$mysqli->connect_error, si esta instruccion da true retorno un mensaje de error. */
 $mysqli = new mysqli("localhost", "root", "", "listadequehaceres");
-if($mysqli->connect_error) {
-  exit('No se puede conectar a la base de datos');
-}
-$name = $_GET['q'];
-
-function insertaItem($name,$mysqli){
-  $sql = "INSERT INTO listdeitems (Nombre) VALUES ('$name')";
-  if (mysqli_query($mysqli, $sql)) {
-    echo "Nuevo registro creado con éxito";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+  if($mysqli->connect_error) {
+    exit('No se puede conectar a la base de datos');
   }
-}
-insertaItem($name,$mysqli);
+
+  $name = $_GET['q'];
+  /*La funcion insertaItem, genera un registro en la tabla listdeitems.*/
+  function insertaItem($name,$mysqli){
+    $sql = "INSERT INTO listdeitems (Nombre) VALUES ('$name')";
+    if (mysqli_query($mysqli, $sql)) {
+      echo "Nuevo registro creado con éxito";
+    } else {
+      echo "No se genero el registro: " . $sql . "<br>" . mysqli_error($mysqli);
+    }
+  }
+  insertaItem($name,$mysqli);
 ?>
