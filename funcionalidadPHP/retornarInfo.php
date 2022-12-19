23f5b0a9c24153f66dd8a92eec5823996ed7fbad
Ale Rangel <31944093+alecy1@users.noreply.github.com>
2022-12-18 22:31:34 -0600

    Eliminado id con foreach en la tabla listdeitems de la base de datos

--- funcionalidadPHP/retornarInfo.php
+++ funcionalidadPHP/retornarInfo.php
@@ -6,53 +6,20 @@ $mysqli = new mysqli("localhost", "root", "", "listadequehaceres");
     exit("No se puede conectar a la base de datos");
   }
 
-$name = $_GET['q'];
-if (isset($_GET['q'])){ 
-    switch ($name){
-      /*Se llama a la funcion mostrarRegistros($mysqli), para desplegar la lista de registros.*/
-      case 'mostrar':
-        mostrarRegistros($mysqli);
-        break; 
-      /*Se llama a la funcion eliminar_registro, para eliminar registro de la tabla listdeitems */
-      case 'eliminar';
-        eliminar_registro($mysqli);
-        break;
-        // default;
-        // insertaItem($name,$mysqli);
-        // break;
-    }
-}
-
-function insertaItem($name,$mysqli){
-  $sql = "INSERT INTO listdeitems (Nombre) VALUES ('$name')";
-  if (mysqli_query($mysqli, $sql)) {
-    echo "New record created successfully";
-  } else {echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);}
-}
-
-/*En la funcion mostrarRegistros, genero un SELECT de la tabla listdeitems, para mostrar los 
-registros o lista de quehaceres en la pagina */
-function mostrarRegistros($mysqli){
-  $retorna ="";
-  $sql = "SELECT Id, Nombre FROM listdeitems";
-  $result = $mysqli->query($sql);
-  if ($result->num_rows > 0) {
-    $contador=1;
-    while($row = $result->fetch_assoc()) {
-      $retorna .= "<li>".$row["Nombre"]."</li>";
-    }
-  } else {echo "No hay items ingresados";}
-  echo $retorna;
-}
-
-/*En la funcion eliminar_registro, codifique un DELETE para la tabla listdeitems y eliminar los registros */
+/*Eliminado id con foreach en la tabla listdeitems de la base de datos*/
 function eliminar_registro($mysqli){
-  $sql = "DELETE FROM listdeitems";
-  $result = $mysqli->query($sql);
-  echo "Se elimino la lista de quehaceres";
+  if(!empty($_POST["item"])){
+    foreach($_POST['item'] as $items)
+    {
+      if($mysqli->query("DELETE FROM listdeitems WHERE Id='$items'"))
+      {
+        echo "Se elimino el registro: ".$items."<br>";
+      }
+    }
+  }else{ echo "Selecciona una opcion, para eliminar";}
 }
 
-
-
+eliminar_registro($mysqli);
+$mysqli->close();
 ?>
 
